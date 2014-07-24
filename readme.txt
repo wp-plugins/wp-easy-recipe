440a727a8c4164df66558bd17f8693780673bedf
india-web-developer <india-web-developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 12:23:50 +0000

    release version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-recipe/trunk@954109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,18 +1,25 @@
 === WP EASY RECIPE===
 Contributors: india-web-developer
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WN785E5V492L4
-Tags: WP Easy Recipe,Easy Recipe,Recipe,Simple Recipe,Wordpress Recipe Plugin
+Tags: WP Easy Recipe,Easy Recipe,Recipe,Simple Recipe
 Requires at least: 2.8
 Tested up to: 3.9.1
-Stable tag: 1.0
+Stable tag: 1.1
 
 Add "WP EASY RECIPE" on your website.
 
 == Description ==
 
-"WP EASY RECIPE" is a very simple plugin for add to "Recipe" content with category and tags on your site in a easy way.
+"WP EASY RECIPE" is a very simple plugin for add to "Recipe" content with seprate category and tags on your site in a easy way. This is fully responsive plugin
 
 Use [wp_recipe_list_page] sortcode for reterive all the recipe pages
+
+=Features=
+*Shortcode [wp_easy_recipe_slider] for recipe feature image slider (responsive)
+*Shortcode [wer_recipe_of_the_day] for get the "Recipe Of The Day"
+*Shortcode [wer_ten_best_recipe] for get the "10 Best Recipe"
+*Option for add .HTML In URL
+*Image Upload Option On Category
 
 == Installation ==
 
@@ -24,7 +31,7 @@ Step 2. Activate the plugin through the Plugins menu in WordPress
 
 1.How publish all the recipe post on any spefic page?
 
-Answer: Create a new page and add [wp_recipe_list_page] sortcode for get all the recipe pages
+Answer: Create a new page and add [wp_recipe_list_page] sortcode 
 
 2.How update the social media content on details page?
 
@@ -35,13 +42,16 @@ Answer: Find the "WP Easy Recipe" settings from admin ("Settings" >> "WP Easy Re
 
 1. screenshot-1.png
 
-1. screenshot-2.png
-
-1. screenshot-3.png
-
-1. screenshot-4.png
 
 == Changelog == 
+
+= 1.1 = 
+
+*Added shortcode [wp_easy_recipe_slider] for recipe feature image slider (responsive)
+*Added shortcode [wer_recipe_of_the_day] for get the "Recipe Of The Day"
+*Added shortcode [wer_ten_best_recipe] for get the "10 Best Recipe"
+*Added an option for add .HTML In URL
+* Image Upload Option On Recipe Category
 
 = 1.0 = 
  * First stable release 
--- wp-easy-recipe.php
+++ wp-easy-recipe.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Easy Recipe
 Plugin URI: http://raghunathgurjar.wordpress.com
 Description: "WP Easy Recipe" is a very simple plugins for manage the recipe content on your site. 
-Version: 1.0
+Version: 1.1
 Text Domain: raghunath
 Author: Raghunath Gurjar
 Author URI: http://www.facebook.com/raghunathprasadgurjar
@@ -30,6 +30,7 @@ add_action('admin_init','wp_easy_recipe_init');
 function wp_easy_recipe_init(){
 
 	register_setting('wp_easy_recipe_options','wpe_shareBtns');
+	register_setting('wp_easy_recipe_options','wpe_dothtml');
 } 
 
 /* Options Form */
@@ -41,18 +42,44 @@ function wp_easy_recipe_admin_option_page(){ ?>
 	<form action="options.php" method="post" id="wp-easy-recipe-admin-form">
 		<table class="wp-easy-recipe">
 			<tr>
-				<td >Add Share Buttons Code:<br>
-				<textarea type="textarea" id="wpe_shareBtns" name="wpe_shareBtns" rows="10" cols="50"  ><?php echo esc_attr(get_option('wpe_shareBtns')); ?></textarea>
+				<td valign="top">Add Share Buttons Code:<br>
+				<textarea type="textarea" id="wpe_shareBtns" name="wpe_shareBtns" rows="10" cols="35"  ><?php echo esc_attr(get_option('wpe_shareBtns')); ?></textarea>
 				</td>
-				<td rowspan="10" valign="top" style="border-left: 1px solid rgb(204, 204, 204); padding-left: 20px;">
-	           /* NOTE: Add [wp_recipe_list_page] this sortcode for get all the recipe pages */
-	            </td>
+					<td>
+		<strong>/* Sortcodes */</strong><br><br>[wp_recipe_list_page] : Use for display all recipe in a single page
+	           <br><br>[cat_menu catid=""] : Use for get the child category link with thumbnail
+	           <br><br>[wp_easy_recipe_slider] : Use for add recipe feature images slider
+	           <br><br>[wer_recipe_of_the_day] :Use for display recipe of the day  
+	           <br><br>[wer_ten_best_recipe] :Use for display 10 best recipe</td>
+				<td rowspan="4" valign="top" style="padding-left: 20px;border-left:1px solid #ccc;">
+					<h2>Plugin Author:</h2>
+					<div style="font-size: 14px;">
+	<img src="<?php echo  plugins_url( 'lib/images/raghu.jpg' , __FILE__ );?>" width="100" height="100"><br><a href="http://raghunathgurjar.wordpress.com" target="_blank">Raghunath Gurjar</a><br><br><a href="mailto:raghunath.0087@gmail.com" target="_blank">Contact Me!</a><br><br>Author Blog <a href="http://raghunathgurjar.wordpress.com" target="_blank">http://raghunathgurjar.wordpress.com</a>
+	<br><br><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WN785E5V492L4" target="_blank" style="font-size: 17px; font-weight: bold;">Donate for this plugin</a><br><br>
+	My Other Plugins:<br>
+	<ul>
+		<li><a href="https://wordpress.org/plugins/simple-testimonial-rutator/" target="_blank">Simple Testimonial Rutator(Responsive)</a></li>
+		<li><a href="https://wordpress.org/plugins/wp-easy-recipe/" target="_blank">WP Easy Recipe</a></li>
+		</ul>
+
+	</div></td>
 			</tr>	
-			
+			<tr scope="row">
+				<td valign="top"><label for="wpe_dothtml"><strong style="font-size:16px;">Add .html in url: </strong></label>
+				<select type="textarea" id="wpe_dothtml" name="wpe_dothtml" >
+					<option value="no" <?php if(get_option('wpe_dothtml')=='no'){echo 'selected="selected"';} ?>>No</option><option value="yes" <?php if(get_option('wpe_dothtml')=='yes'){echo 'selected="selected"';} ?>>Yes</option></select><br>(<strong>Example:</strong> http://your-domain/recipe/demo.html)
+				</td>
+				<td valign="top">&nbsp;</td>
+				<td valign="top">&nbsp;</td>
+			</tr>	
 			<tr>
-				<td><?php echo get_submit_button('Save Settings','button-primary','submit','','');?></td>
+				<td colspan="3">&nbsp;</td>
 			</tr>	
-			<tr><td >&nbsp;</td></tr>		
+			<tr><td colspan="3">&nbsp;</td></tr>	
+			<tr>
+				<td ><?php echo get_submit_button('Save Settings','button-primary','submit','','');?></td><td>&nbsp;</td><td>&nbsp;</td>
+			</tr>	
+			<tr><td colspan="3">&nbsp;</td></tr>		
 		</table>
     <?php settings_fields('wp_easy_recipe_options'); ?>
 	</form>
@@ -94,6 +121,7 @@ if( function_exists('register_uninstall_hook') )
 //Delete all Custom Tweets options after delete the plugin from admin
 function wp_easy_recipe_uninstall(){
 	delete_option('wpe_shareBtns');
+	delete_option('wpe_dothtml');
 } 
 
 ?>
